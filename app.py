5538e71c4ef7cbf3c496c99535ed6339bb9a1e68
zhammer <zachary_hammer@alumni.brown.edu>
2020-08-08 20:10:11 -0400

    setup basic app test

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def hello():
+  return "Hello world!"
+
+if __name__ == "__main__":
+  app.run(port=os.getenv("PORT", "7070"))
